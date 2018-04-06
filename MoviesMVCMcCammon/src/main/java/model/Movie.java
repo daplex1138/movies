8dffe89312344a21c4fbbfdc64cfd8be80024f86
jrchongo <36547496+jrchongo@users.noreply.github.com>
2018-04-06 15:40:43 -0500

    Initial classes for director and movie

---
+++ MoviesMVCMcCammon/src/main/java/model/Movie.java
@@ -0,0 +1,98 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="movie")
+public class Movie {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String year;
+	private String rating;
+	private String genre;
+	private int duration;
+	@ManyToOne
+	@JoinColumn (name = "director_id")
+	private int directorId;
+	
+	public Movie() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Movie(String title, String year, String rating, String genre, int duration, int directorId) {
+		super();
+		this.title = title;
+		this.year = year;
+		this.rating = rating;
+		this.genre = genre;
+		this.duration = duration;
+		this.directorId = directorId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getYear() {
+		return year;
+	}
+
+	public void setYear(String year) {
+		this.year = year;
+	}
+
+	public String getRating() {
+		return rating;
+	}
+
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public int getDirectorId() {
+		return directorId;
+	}
+
+	public void setDirectorId(int directorId) {
+		this.directorId = directorId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie [id=" + id + ", title=" + title + ", year=" + year + ", rating=" + rating + ", genre=" + genre
+				+ ", duration=" + duration + ", directorId=" + directorId + "]";
+	}
+}
